1ccf96cbc942b5c32ebbd1a6c71c9ff8cb1c37b5
Muhammad David Kurniawan <kurniawanflix@gmail.com>
2022-12-10 13:04:01 +0700

    add api

---
+++ src/components/movie-card.js
@@ -0,0 +1,30 @@
+import { Box, Image } from "@chakra-ui/react";
+import React from "react";
+
+const MovieCard = ({ title, year, poster }) => {
+  return (
+    <Box p={["1em", "1em", "1em", "1em", "1.5em"]}>
+      <Box boxShadow="md" w="250px" objectFit="contain" position="relative">
+        <Image src={poster} fit="cover" w="full" h="400px" alt="movie-title" />
+        <Box
+          py="5px"
+          minHeight="60px"
+          position="absolute"
+          bottom={0}
+          w="full"
+          fontSize="md"
+          color="white"
+          fontWeight="semibold"
+          bg="black"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          children={`${title} (${year})`}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default MovieCard;
